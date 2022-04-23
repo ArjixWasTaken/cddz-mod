2a88f2547247ceb6e9e7e411ab9028ab053c93c1
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2022-04-23 22:13:11 +0300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CloneDroneInTheDangerZone
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInProcess("Clone Drone in the Danger Zone.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "com.arjix.cddz.mods";
+        private const string modName = "BlaBla";
+        private const string modVersion = "69.4.20";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin '{modGUID}' is loaded!");
+            harmony.PatchAll();
+        }
+
+        void Update()
+        {
+            if ((Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)) && Input.GetKeyUp(KeyCode.B))
+            {
+                // Give 5 skill points to the player
+                Singleton<UpgradeManager>.Instance.SetAvailableSkillPoints(
+                    Singleton<UpgradeManager>.Instance.GetAvailableSkillPoints() + 5
+                );
+                Logger.LogInfo("Gave 5 skill points to the player");
+            } else if ((Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)) && Input.GetKeyUp(KeyCode.J)) {
+                // toggle infinite energy
+                Data.HasInfiniteEnergy = !Data.HasInfiniteEnergy;
+                CharacterTracker.Instance.GetPlayer().GetEnergySource().HasInfiniteEnergy = Data.HasInfiniteEnergy;
+                Logger.LogInfo($"Infinite Energy: {Data.HasInfiniteEnergy}!");
+            }
+        }
+    }
+
+    public static class Data
+    {
+        public static bool HasInfiniteEnergy = false;
+    }
+
+    [HarmonyPatch]
+    class Steam_patcher
+    {
+        // Disables the steam integration with the game.
+        // TODO: Make this configurable from an options file.
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "Initialize")]
+        static bool Start(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(EnergySource), "Awake")]
+    class EnergySourcePatches
+    {
+        [HarmonyPrefix]
+        static void setInfiniteEnergyRef(ref bool ___HasInfiniteEnergy)
+        {
+            ___HasInfiniteEnergy = Data.HasInfiniteEnergy;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cddz-mod
+# Clone Drone In The Danger Zone mod
+
+As the header says, this is a mod for the game "Clone Drone In The Danger Zone".
+It targets the version (5/12/2021), so if it doesn't work, it's probably because you're using an older or newer version.
+
+Current features are:
+- Infinite Energy (can be toggled on/off using Ctrl+J)
+- Get 5 skill points every time you press Ctrl+B
+
+I disabled the steam integration so you can't use this unfair mod on multiplayer.
+
+Any contributions are welcome.
+
+Big thanks to [BepInEx](https://docs.bepinex.dev/index.html) for existing and making the process of creating a mod enjoyable.
+
+## Installation
+
+- Download the 32bit build of BepInEx [here](https://github.com/BepInEx/BepInEx/releases/download/v5.4.19/BepInEx_x86_5.4.19.0.zip)
+- Extract the contents of the zip file into the game's directory.
+- Download the dll of this mod from the releases (or you can manually build it)
+- Place the mod's dll into the plugins folder located in the BepInEx directory which in turn is located in the game's directory.
